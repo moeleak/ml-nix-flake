ff1a15379d0a10bbcd25f3105099c4c3abef0b13
MoeLeak <i@leak.moe>
2025-09-24 11:46:25 +0800

    feat: Remove something that is not necessary and may be conflicts with other files

--- README.md
+++
@@ -1,7 +0,0 @@
-# ml-nix-flake
-A simple nix flake to start ML env out of the box
-
-## Usage
-```
-nix develop
-```
--- flake.nix
+++ flake.nix
@@ -2,7 +2,7 @@
   description = "dev shell with uv and cuda out of the box";
 
   inputs = {
-    nixpkgs.url = "github:NixOS/nixpkgs/nixos-24.11";
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
     nix-gl-host.url = "github:moeleak/nix-gl-host";
   };
 
